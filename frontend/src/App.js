cc83192dec35e19bd34e7ecf345228d3a34448b9
nouri <60990203+nouri89@users.noreply.github.com>
2020-11-20 16:12:43 +0000

    another footer fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,33 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import { Classes, Class, Header, PrivateRoute, Home, } from "./components";
+import { Classes, Class, Header, PrivateRoute, Home } from "./components";
 import { ProvideAuth } from "./hooks/use-auth";
-import Footer from "./components/Footer/Footer"
-
-
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return (
-    <ProvideAuth>
-      <Router>
-        <Header />
+	return (
+		<ProvideAuth>
+			<Router>
+				<Header />
 
-        <Container>
-          <Switch>
-            <Route exact path="/">
+				<Container>
+					<Switch>
+						<Route exact path="/">
               <Home />
-            </Route>
-            <PrivateRoute path="/classes">
-              <Classes />
-            </PrivateRoute>
-            <PrivateRoute path="/class/:className">
-              <Class />
-            </PrivateRoute>
-          </Switch>
-        </Container>
-        <Footer />
-      </Router>
-    </ProvideAuth>
-  );
+              
+						</Route>
+						<PrivateRoute path="/classes">
+							<Classes />
+						</PrivateRoute>
+						<PrivateRoute path="/class/:className">
+							<Class />
+						</PrivateRoute>
+					</Switch>
+				</Container>
+				<Footer />
+			</Router>
+		</ProvideAuth>
+	);
 }
 
 export default App;
