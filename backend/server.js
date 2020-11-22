9e8da514ee62e227a6c1be2d3c8193920970bab7
davibaweja <davibaweja@gmail.com>
2020-11-22 17:38:59 +0000

    added end point cyf-classes
    this end point gives all the classes from different regions.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,33 @@
 const express = require('express');
+const cors = require("cors");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hack Slash');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+const { Pool, Client } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "cyf_student_tracker",
+  password: "cyf",
+  port: "5432",
+});
+
+app.get("/cyf-classes", function (req, res) {
+  let selectCohorts = `select * from cohort `;
+  pool.query(selectCohorts, (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    if (results.rows.length > 0) {
+      res.json(results.rows);
+    }
+  });
 });
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
