0666bc1174f6e8e2ada725120b34a90afcc03963
nouri <60990203+nouri89@users.noreply.github.com>
2020-11-23 11:26:58 +0000

    updating classes

--- frontend/src/components/ClassesPage/Classes.js
+++ frontend/src/components/ClassesPage/Classes.js
@@ -1,17 +1,39 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import { fakeData } from '../../fakeData';
+import { fakeData } from "../../fakeData";
+import "./Classes.scss";
 
 const Classes = () => {
-  return (
-    <ul>
-      {fakeData.map((fakeClass, index) => (
-        <li key={index}>
-          <Link to={`/class/${fakeClass.name}`}>{fakeClass.name}</Link>
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<div>
+			<h1>Select a class..</h1>
+			<div className="row" id="main">
+				{fakeData.map((fakeClass) => (
+					<div className="col-6 rounded ">
+						<Link to={`/class/${fakeClass.name}`}>
+							<img
+								src={fakeClass.image}
+								className="rounded mx-auto d-block"
+								id="cityImage"
+								alt="photo of city"
+							/>
+							<h2 id="className">{fakeClass.name}</h2>
+						</Link>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Classes;
+
+/*
+	<ul>
+				{fakeData.map((fakeClass, index) => (
+					<li key={index}>
+						<Link to={`/class/${fakeClass.name}`}><img src= {fakeClass.image}></img></Link>
+					</li>
+				))}
+        </ul>
+*/
--- frontend/src/fakeData.js
+++ frontend/src/fakeData.js
@@ -1,31 +1,59 @@
 // TODO to add students data also
 
 export const fakeData = [
-  {
-    name: 'westmidlands1',
-    students: 35,
-    coordinators: ['Claire', 'Aaishah'],
-    attendance: '92.3%',
-    score: 8.2,
-    start: '01-03-20',
-    end: '01-01-20',
-  },
-  {
-    name: 'westmidlands2',
-    students: 32,
-    coordinators: ['Claire', 'Aaishah'],
-    attendance: '82.3%',
-    score: 9.2,
-    start: '01-10-20',
-    end: '01-08-20',
-  },
-  {
-    name: 'london6',
-    students: 43,
-    coordinators: ['Chris', 'Jon'],
-    attendance: '92.3%',
-    score: 7.2,
-    start: '01-03-19',
-    end: '01-01-20',
-  },
+	{
+		name: "westmidlands 1",
+		students: 35,
+		coordinators: ["Claire", "Aaishah"],
+		attendance: "92.3%",
+		score: 8.2,
+		start: "01-03-20",
+		end: "01-01-20",
+		image:
+			"https://avisassets.abgemea.com/dam/jcr:ca43cc10-cfed-4364-b116-a5b4091a3ee7/birmingham-cityscape-inspirational-desktop.jpg",
+	},
+	{
+		name: "westmidlands 2",
+		students: 32,
+		coordinators: ["Claire", "Aaishah"],
+		attendance: "82.3%",
+		score: 9.2,
+		start: "01-10-20",
+		end: "01-08-20",
+		image:
+			"https://avisassets.abgemea.com/dam/jcr:ca43cc10-cfed-4364-b116-a5b4091a3ee7/birmingham-cityscape-inspirational-desktop.jpg",
+	},
+	{
+		name: "London 6",
+		students: 43,
+		coordinators: ["Chris", "Jon"],
+		attendance: "92.3%",
+		score: 7.2,
+		start: "01-03-19",
+		end: "01-01-20",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg/1000px-Palace_of_Westminster_from_the_dome_on_Methodist_Central_Hall_%28cropped%29.jpg",
+	},
+	{
+		name: "Glasgow",
+		students: 43,
+		coordinators: ["Chris", "Jon"],
+		attendance: "92.3%",
+		score: 7.2,
+		start: "01-03-19",
+		end: "01-01-20",
+		image:
+			"https://i1.wp.com/www.hisour.com/wp-content/uploads/2018/05/Architecture-in-Glasgow.jpg?fit=1024%2C640&ssl=1",
+	},
+	{
+		name: "Manchester",
+		students: 43,
+		coordinators: ["Chris", "Jon"],
+		attendance: "92.3%",
+		score: 7.2,
+		start: "01-03-19",
+		end: "01-01-20",
+		image:
+			"https://feverweb.imgix.net/cities/manchester-1125x551.jpg?auto=format&auto=compress",
+	},
 ];
