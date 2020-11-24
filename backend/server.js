18fa63274adc0632bf1d7211f17763f1280ef450
Your Name <hiba.moh@yahoo.com>
2020-11-24 14:58:09 +0000

    Attendance- endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,32 @@ const express = require ('express');
 const PORT = process.env.PORT || 4000;
 const app = express ();
 
-app.get ('/', (req, res) => {
-  res.send ('Hack Slash');
+
+app.post ('/attendance', (req, res) => {
+  let data = req.body;
+  console.log (data);
+  data.forEach (obj => {
+    pool.query (
+      `insert into attendance(status,student_id,cohort_name,week ,module,mentor_id,date)
+            values($1,$2,$3,$4,$5,$6,$7)`,
+      [
+        obj.status,
+        obj.student_id,
+        obj.cohort_name,
+        obj.week,
+        obj.module,
+        obj.mentor_id,
+        obj.date,
+      ],
+      (err, results) => {
+        if (err) {
+          throw err;
+        }
+        console.log (results.rows);
+        res.send ('successful');
+      }
+    );
+  });
 });
 
-app.listen (PORT, () => console.log (`Server is listening on port ${PORT}`));
+
