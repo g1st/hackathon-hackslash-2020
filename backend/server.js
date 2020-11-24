adc6bc90f539a86a79bb1c021514ed37e1f58004
Your Name <hiba.moh@yahoo.com>
2020-11-24 15:01:25 +0000

    End points for the classes page and the mentor page

--- backend/server.js
+++ backend/server.js
@@ -31,3 +31,59 @@ app.post ('/attendance', (req, res) => {
 });
 
 
+pp.get ('/cyf-classes', function (req, res) {
+  let selectCohorts = `select * from cohort `;
+  pool.query (selectCohorts, (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    if (results.rows.length > 0) {
+      res.json (results.rows);
+    }
+  });
+});
+
+app.get ('/modules', function (req, res) {
+  let selectModules = `select * from module`; //modify this line
+  pool.query (selectModules, (err, results) => {
+    if (err) {
+      res.json (null);
+      throw err;
+    }
+
+    if (results.rows.length > 0) {
+      res.json (results.rows);
+    }
+  });
+});
+
+app.get ('/cyf-classes/:className/students', function (req, res) {
+  const {className} = req.params;
+  let selectStudents = `select * from student where cohort_name=$1 `;
+  pool.query (selectStudents, [className], (err, results) => {
+    if (err) {
+      res.json (null);
+      throw err;
+    }
+    if (results.rows.length > 0) {
+      res.json (results.rows);
+    }
+  });
+});
+
+app.get ('/cyf-classes/:className/attendance', function (req, res) {
+  const {className} = req.params;
+  let selectAttendance = `select * from attendance where cohort_name=$1 `;
+  pool.query (selectAttendance, [className], (err, results) => {
+    if (err) {
+      res.json (null);
+      throw err;
+    }
+    if (results.rows.length > 0) {
+      res.json (results.rows);
+    }
+  });
+});
+
+
